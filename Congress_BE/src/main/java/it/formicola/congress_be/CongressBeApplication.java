53a46ea7c6e678d06bb254be7a7b3de5b0e82a09
Alessandro Formicola <formicola04alex@gmail.com>
2026-09-04 10:53:18 +0200

    Initial Setup

---
+++ Congress_BE/src/main/java/it/formicola/congress_be/CongressBeApplication.java
@@ -0,0 +1,13 @@
+package it.formicola.congress_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongressBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongressBeApplication.class, args);
+	}
+
+}
---
+++ Congress_BE/src/test/java/it/formicola/congress_be/CongressBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.formicola.congress_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongressBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
