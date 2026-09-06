f464bc2f252e3f396466f2a947005f7c31b1cefb
Al3x370 <formicola04alex@gmail.com>
2026-09-06 15:47:24 +0200

    Added routes to the application

--- Congress_FE/src/app/app.routes.ts
+++ Congress_FE/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'participants',
+    title: 'Congresso',
+    loadComponent: () =>
+      import('./components/participants/participants').then(
+        (component) => component.Participants,
+      ),
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'participants' }
+];
